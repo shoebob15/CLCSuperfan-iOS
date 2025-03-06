275843000e993b70ff76daf4fc7bd761f7b54216
Brennan Reinhard <brennanreinhard15@gmail.com>
2025-03-05 21:35:48 -0600

    Initial Commit

---
+++ CLCSuperfan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CLCSuperfan
+//
+//  Created by Brennan Reinhard on 3/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
