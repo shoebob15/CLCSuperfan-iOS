f17b69af87e9a55dd21402377e2bfa9836a2a440
Brennan Reinhard <brennanreinhard15@gmail.com>
2025-03-07 08:08:16 -0600

    appdelegate google sign in

--- CLCSuperfan/AppDelegate.swift
+++ CLCSuperfan/AppDelegate.swift
@@ -6,18 +6,42 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    func application(
+      _ app: UIApplication,
+      open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+      var handled: Bool
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+      handled = GIDSignIn.sharedInstance.handle(url)
+      if handled {
         return true
-    }
+      }
 
-    // MARK: UISceneSession Lifecycle
+      // Handle other custom URL types.
+
+      // If not handled by this app, return false.
+      return false
+    }
+    
+    func application(
+      _ application: UIApplication,
+      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+      GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+        if error != nil || user == nil {
+          // Show the app's signed-out state.
+        } else {
+          // Show the app's signed-in state.
+        }
+      }
+      return true
+    }
+    
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
