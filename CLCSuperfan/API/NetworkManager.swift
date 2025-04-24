8bb5ae54599d8f13cebd9bc47c06b09eb919b8c3
Brennan Reinhard <brennanreinhard15@gmail.com>
2025-04-23 19:00:07 -0700

    add note

--- CLCSuperfan/API/NetworkManager.swift
+++ CLCSuperfan/API/NetworkManager.swift
@@ -19,13 +19,16 @@ enum NetworkError: Error {
 class NetworkManager {
     static let shared = NetworkManager()
 
-     // this url is the PRODUCTION url. use this if you are running on a physical device
-     // (or simulator), or if this is a production build.
-     static let apiUrl = "https://illegal-adrianna-clc-superfan-e909ede6.koyeb.app/"
+    // this url is the PRODUCTION url. use this if you are running on a physical device
+    // (or simulator), or if this is a production build.
+    // NOTE: this may be blocked on school networks (idk yet), so I might have to
+    // talk to IT about getting it whitelisted
+     
+    static let apiUrl = "https://illegal-adrianna-clc-superfan-e909ede6.koyeb.app/"
     
-     // this url is the DEBUG url. this is used when running the server locally in
-     // IntelliJ. use this url if you are testing backend changes or running in the sim
-     // static let apiUrl = "http://localhost:1924"
+    // this url is the DEBUG url. this is used when running the server locally in
+    // IntelliJ. use this url if you are testing backend changes or running in the sim
+    // static let apiUrl = "http://localhost:1924"
     
     private let session = URLSession.shared
     
