e44bf7c1cbb273a6045d38c6efb4edce5ec9a074
LoganGough <logan.gough26@d255.org>
2025-04-28 08:19:46 -0500

    leaderboard should be working

---
+++ CLCSuperfan/LeaderboardCell2.swift
@@ -0,0 +1,18 @@
+//
+//  LeaderboardCell2.swift
+//  CLCSuperfan
+//
+//  Created by LOGAN GOUGH on 4/23/25.
+//
+
+import Foundation
+import UIKit
+
+class LeaderboardCell2: UITableViewCell{
+    
+    @IBOutlet weak var lab1: UILabel!
+    
+    @IBOutlet weak var lab2: UILabel!
+    
+    
+}
--- CLCSuperfan/LeaderboardViewController.swift
+++ CLCSuperfan/LeaderboardViewController.swift
@@ -7,10 +7,19 @@
 
 import UIKit
 
-class LeaderboardViewController: UIViewController{
+
+class LeaderboardViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+
     var topTen = [User]()
     var username = ""
     var point = ""
+    var lastInitial = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +34,22 @@ class LeaderboardViewController: UIViewController{
         }
         
         
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell") as! LeaderboardCell2
+        var i = lastInitial.index(lastInitial.startIndex, offsetBy: 1)
+        var blat = "\(topTen[indexPath.row].firstName)  \(i)"
+        var blah = "\(topTen[indexPath.row].points) \(i)"
+        cell.lab1?.text = "\(blat)"
+        cell.lab2?.text = "\(blah)"
+        
+        return cell
     }
 
     
